15fbbf9a62e22df53e2588420b856bb063a80524
Max Toh <ctzongde@hotmail.com>
2026-02-05 00:24:32 +0800

    Updated returnVisitorCount.js with visitor counter function

--- returnVisitorCount.js
+++ returnVisitorCount.js
@@ -1,9 +1,13 @@
 console.log("Javascript file loaded: returnVisitorCounter.js");
 
 const visitorCounter = document.getElementById("visitorCount");
-visitorCounter.innerHTML = "This proves that the Script is able to change the section in HTML file using the ID selector."
 
-fetch("https://jsonplaceholder.typicode.com/")
-.then(response => response.text())
-.then(data => {console.log(data);
-}); 
+// fetch the visitor count from the API invoke URL that would return a promise
+fetch("https://u0ulp8txth.execute-api.ap-southeast-1.amazonaws.com/count")
+// open the response as json 
+.then(Response => Response.json())
+// set the innerHTML of the visitorCounter element to the count value from the json response
+.then(data => {visitorCounter.innerHTML = data.count + " visitors have visited this resume.";
+     console.log(data.count + " visitors have visited this resume.");})
+// catch errors and log them to the console
+.catch(error => console.error("Error fetching visitor count:", error));
