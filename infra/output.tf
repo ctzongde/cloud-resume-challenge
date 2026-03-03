b3cb0fea5ae908634874d377a4cc602b0f45ebaf
Max Toh <ctzongde@hotmail.com>
2026-03-03 23:52:58 +0800

    infra: added output.tf file to store API url leading to the Lambda function

---
+++ infra/output.tf
@@ -0,0 +1,5 @@
+output "visitor_counter_api_url" {
+    # Output the URL of the API Gateway endpoint to access the visitor counter API
+    # The argument value is constructed using the API endpoint from the aws_apigatewayv2_api resource and appending the /count route_key defined in the aws_apigatewayv2_route resource
+    value = "${aws_apigatewayv2_api.visitor_counter_api.api_endpoint}/count"
+}
